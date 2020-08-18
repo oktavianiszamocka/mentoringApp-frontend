a2fea4b906d2a27ee9342acc8023de9048e8ca5e
Piotr Gago <gagopiotr@gmail.com>
2020-08-18 10:13:52 +0200

    Added proptypes and default props

--- src/screens/shared/components/Avatar.js
+++ src/screens/shared/components/Avatar.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledDiv = styled.div`
   display: flex;
 `;
 
-const StyledAvatar = styled.img`
-  display: 500px;
-  border-radius: ${(props) => props.imgTheme.borderRadius};
-  width: ${(props) => props.imgTheme.width};
+const StyledImg = styled.img`
+  border-radius: 5px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.width};
   box-shadow: 1px 1px 2px 0px rgba(135, 135, 135, 1);
 `;
 
 const StyledSpan = styled.span`
   font-family: 'Open Sans Condensed', sans-serif;
-  font-size: ${(props) => props.spanTheme.fontSize};
-  margin-left: ${(props) => props.spanTheme.marginLeft};
-  margin-top: ${(props) => props.spanTheme.marginTop};
+  font-size: 1rem;
+  margin-left: 1rem;
+  margin-top: 1rem;
 `;
 
-const Avatar = ({ firstName, lastName, imageUrl, imgTheme, spanTheme }) => {
+const Avatar = ({ firstName, lastName, imageUrl, width }) => {
   return (
     <StyledDiv>
-      <StyledAvatar src={imageUrl} imgTheme={imgTheme} />
-      <StyledSpan spanTheme={spanTheme}>{`${firstName} ${lastName}`}</StyledSpan>
+      <StyledImg src={imageUrl} width={width} />
+      <StyledSpan>{`${firstName} ${lastName}`}</StyledSpan>
     </StyledDiv>
   );
+};
+
+Avatar.defaultProps = {
+  imageUrl: '',
+  width: '50px',
+};
+
+Avatar.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string,
+  width: PropTypes.string,
 };
 
 export default Avatar;
