a7774f65b09b8125cfff93857260afea7887f139
Piotr Gago <gagopiotr@gmail.com>
2020-08-18 10:03:19 +0200

    Added eslint configuration

--- src/screens/shared/components/Avatar.js
+++ src/screens/shared/components/Avatar.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
-const StyledDiv=styled.div`
+const StyledDiv = styled.div`
   display: flex;
 `;
 
-
-const StyledAvatar=styled.img`
+const StyledAvatar = styled.img`
   display: 500px;
-  border-radius: ${props => props.imgTheme.borderRadius};
-  width: ${props => props.imgTheme.width};
-  box-shadow: 1px 1px 2px 0px rgba(135,135,135,1);
-  `;
-
-const StyledSpan=styled.span`
-  font-family: 'Open Sans Condensed', sans-serif;
-  font-size: ${props => props.spanTheme.fontSize};
-  margin-left: ${props => props.spanTheme.marginLeft};
-  margin-top: ${props => props.spanTheme.marginTop};
+  border-radius: ${(props) => props.imgTheme.borderRadius};
+  width: ${(props) => props.imgTheme.width};
+  box-shadow: 1px 1px 2px 0px rgba(135, 135, 135, 1);
 `;
 
+const StyledSpan = styled.span`
+  font-family: 'Open Sans Condensed', sans-serif;
+  font-size: ${(props) => props.spanTheme.fontSize};
+  margin-left: ${(props) => props.spanTheme.marginLeft};
+  margin-top: ${(props) => props.spanTheme.marginTop};
+`;
 
-const Avatar=({firstName, lastName, imageUrl, imgTheme, spanTheme})=>{
+const Avatar = ({ firstName, lastName, imageUrl, imgTheme, spanTheme }) => {
   return (
     <StyledDiv>
       <StyledAvatar src={imageUrl} imgTheme={imgTheme} />
