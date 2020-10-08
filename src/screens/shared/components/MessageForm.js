7fd31f1705e1fbb585b453758a448278b8767be5
unknown <joanna.bienkowska8@gmail.com>
2020-10-08 16:39:44 +0200

    Add more styling to Message component

--- src/screens/shared/components/MessageForm.js
+++ src/screens/shared/components/MessageForm.js
@@ -2,34 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 import { useFormik } from 'formik';
 import Button from '@material-ui/core/Button';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 import { IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 const StyledNewMessageContainer = styled.div`
-  height: 4rem;
+  height: 4.5rem;
   width: 45rem;
   margin: 7px;
   box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.2);
   background: #fff;
 `;
 
-const StyledInput = styled.input`
-  margin: 15px;
-  width: 30rem;
-  border-radius: 0.5rem;
-  border: 1px solid #dcdcdc;
-`;
-
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
-      margin: theme.spacing(1),
+      margin: theme.spacing(-1),
     },
   },
   input: {
     display: 'none',
   },
+  inField: {
+    width: '30rem',
+    marginLeft: '10px',
+    marginTop: '8px',
+    marginRight: '0.4rem',
+  },
+  inPhoto: {
+    marginTop: '10px',
+    marginRight: '4rem',
+  },
+  button: { marginTop: '1rem' },
 }));
 
 export default function MessageForm() {
@@ -37,7 +42,7 @@ export default function MessageForm() {
 
   const formik = useFormik({
     initialValues: {
-      message: 'Type your message',
+      message: '',
     },
     onSubmit: (values) => {
       console.log('Message:', values);
@@ -47,7 +52,10 @@ export default function MessageForm() {
   return (
     <StyledNewMessageContainer>
       <form onSubmit={formik.handleSubmit}>
-        <StyledInput
+        <TextField
+          className={classes.inField}
+          label="Message"
+          variant="outlined"
           type="text"
           id="message"
           name="message"
@@ -56,11 +64,16 @@ export default function MessageForm() {
         />
         <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
         <label htmlFor="icon-button-file">
-          <IconButton color="black" aria-label="upload picture" component="span">
-            <PhotoCamera />
+          <IconButton
+            className={classes.inPhoto}
+            color="black"
+            aria-label="upload picture"
+            component="span"
+          >
+            <AddAPhotoIcon fontSize="medium" />
           </IconButton>
         </label>
-        <Button type="submit" variant="outlined" margin="4px">
+        <Button className={classes.button} type="submit" variant="outlined">
           Send
         </Button>
       </form>
