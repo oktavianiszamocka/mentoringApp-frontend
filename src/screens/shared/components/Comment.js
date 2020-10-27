568c51337a10435277e2a190d26fa6a4bb246621
Dilara Aygun <elifdilaa@gmail.com>
2020-10-27 17:22:19 +0300

    Revert "connect withbackend"

--- src/screens/shared/components/Comment.js
+++ src/screens/shared/components/Comment.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Avatar from '../components/Avatar';
 import styled from 'styled-components';
-import moment from 'moment';
+import photo from '../../../assets/images/taylor.jpg';
 
+const user = {
+  firstName: 'Joker',
+  lastName: 'the Kitty',
+  imageUrl: photo.toString(),
+  comment: 'That is cool!!',
+  time: '1hr ago',
+};
 
 const imgTheme = {
   width: '50px',
@@ -18,15 +25,13 @@ const StyledDiv = styled.div`
   display: flex;
 `;
 
-const Comment = ({comment}) => {
-
-  var momentTime = moment(JSON.stringify(comment.createdOn), 'YYYY-MM-DD hh:mm:ss').fromNow();
+const Comment = () => {
   return (
     <StyledDiv>
-      <Avatar {...comment.createdBy} imgTheme={imgTheme} spanTheme={spanTheme} />
+      <Avatar {...user} imgTheme={imgTheme} spanTheme={spanTheme} />
       <div className="media-body p-2 shadow-sm rounded bg-light border">
-        <small className="float-right text-muted">{momentTime}</small>
-        <h6 className="mt-2 mb-4 text-muted">{comment.comment}</h6>
+        <small className="float-right text-muted">{user.time}</small>
+        <h6 className="mt-2 mb-4 text-muted">{user.comment}</h6>
       </div>
     </StyledDiv>
   );
