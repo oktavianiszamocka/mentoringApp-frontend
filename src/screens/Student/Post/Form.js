11b3829aa3c4575d03993972c98bfbca29d198b0
Piotr Gago <gagopiotr@gmail.com>
2020-11-09 19:00:42 +0100

    Styled the form elements

--- src/screens/Student/Post/Form.js
+++ src/screens/Student/Post/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
+import { makeStyle } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import * as Yup from 'yup';
 import Button from '@material-ui/core/Button';
@@ -8,6 +9,7 @@ import styled from 'styled-components';
 
 const StyledSection = styled.section`
   margin: 2rem;
+  padding: 1rem;
   background-color: #f5f5f5;
   border-radius: 5px;
   box-shadow: 1px 1px 2px 0px rgba(135, 135, 135, 1);
@@ -33,18 +35,30 @@ const UpsertPostForm = ({ onNewPostSubmitHandler, user }) => (
   <Formik
     onSubmit={onNewPostSubmitHandler}
     // validationSchema={PostSchema}
-    initialValues={{}}
+    initialValues={{
+      title: '',
+      mainText: '',
+      tags: [],
+    }}
   >
     <Form>
       <StyledSection>
         <div style={{ display: 'flex' }}>
           <StyledImg src={user.imageUrl} width="75px" />
           <div style={{ display: 'inline-grid' }}>
-            <Field as={TextField} name="title" label="title" />
+            <Field as={TextField} name="title" label="Write here your title..." />
           </div>
         </div>
-        <Field as={TextField} name="text" label="text" fullWidth />
-        <Field as={TextField} name="tags" label="tags" fullWidth />
+        <Field
+          as={TextField}
+          name="mainText"
+          maxLength={300}
+          multiline
+          style={{ height: '100px' }}
+          label="Write here your message..."
+          fullWidth
+        />
+        <Field as={TextField} name="tags" label="Write here your tags..." fullWidth />
         <div style={{ textAlignLast: 'right' }}>
           <Button
             style={{ margin: '15px' }}
@@ -63,9 +77,6 @@ const UpsertPostForm = ({ onNewPostSubmitHandler, user }) => (
 
 UpsertPostForm.propTypes = {
   onNewPostSubmitHandler: PropTypes.func.isRequired,
-  user: PropTypes.exact({
-    imageUrl: PropTypes.string.isRequired,
-  }),
   // initialValues: PropTypes.object,
 };
 
