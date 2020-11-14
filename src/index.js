144bcbdc1b3398fc896dec99a868497b350481d9
Elif Aygun <elifdilaa@gmail.com>
2020-11-14 21:43:48 +0300

    Implemented LoginPage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './screens';
-import StudentDashboard from './screens/Student/StudentDashboard';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import MyProfileDashboard from './screens/shared/components/MyProfileDashboard';
+import App from './screens/index';
 
-ReactDOM.render(<MyProfileDashboard />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
