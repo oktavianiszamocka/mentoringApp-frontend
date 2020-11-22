b4b76d1512af52411999018a4f4f3447e3f26e8e
Elif Aygun <elifdilaa@gmail.com>
2020-11-22 13:23:35 +0300

    modified index.js

--- src/api/index.js
+++ src/api/index.js
@@ -8,7 +8,6 @@ const apiUrl = 'http://localhost:5000/api';
 const apiToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiamFuMTIzIiwiVXNlcklkIjoiMSIsImV4cCI6MTkyMDI5MzcxNCwiaXNzIjoiQSIsImF1ZCI6IkIifQ.7IR6pXXjGJ64lHk5qLGL_utQEWsZQBpEGF_leGw3reA';
 
 axios.defaults.headers.common.Authorization = apiToken;
-
 const getUserId = () => 1;
 const getNotes = () => axios.get(`${apiUrl}/personal-notes/${getUserId()}`);
 const getPosts = () => axios.get(`${apiUrl}/posts`);
