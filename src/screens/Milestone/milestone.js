acade951e343563c1cbd24194e08f6f3fb55ba08
Elif Aygun <elifdilaa@gmail.com>
2021-02-14 22:01:49 +0300

    Milestone component is created

---
+++ src/screens/Milestone/milestone.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class milestone extends Component {
+    render() {
+        return (
+            <div />
+        );
+    }
+}
+
+export default milestone;
