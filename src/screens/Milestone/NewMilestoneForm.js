f546e6d4c840b668d832caa6a58c16cf02aaf2a6
Oktaviani Puspita <44846145+oktavianiszamocka@users.noreply.github.com>
2021-10-25 00:21:28 +0200

    finisih milestone

--- src/screens/Milestone/NewMilestoneForm.js
+++ src/screens/Milestone/NewMilestoneForm.js
@@ -1,89 +1,114 @@
 import React from 'react';
-import { Button, Paper, TextField } from '@material-ui/core';
+import {
+  Button, Paper, TextField, Grid,
+} from '@material-ui/core';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import PropTypes from 'prop-types';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
+
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import logo from '../../assets/images/pja.png';
-import { blue500 } from 'material-ui/styles/colors';
 import { blue50 } from 'material-ui/styles/colors';
-import { StayPrimaryLandscape } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        background: blue50,
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submitButton: {
-        marginTop: '10px',
-      
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    background: blue50,
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submitButton: {
+    marginTop: '10px',
+
+  },
 }));
 
 const MilestoneSchema = Yup.object().shape({
-    note: Yup.string().min(1).max(300).required('Please enter the milestone description'),
+  description: Yup.string().min(1).max(300).required('Please enter the milestone description'),
 });
 
+const MilestoneForm = (props) => {
+  const handleSubmit = async (values, { resetForm }) => {
+    const successCallout = false;
 
-const MilestoneForm = ({ onSubmit, initialValue }) => {
-    const classes = useStyles();
+    await props.onSubmitHandler(values)
+      .then((res) => {
+        resetForm({
+          values: {
+            description: '',
+          },
+        });
+      });
+  };
+  const classes = useStyles();
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
 
-                <h1> New Milestone Step </h1>
+        <h1>
+          {' '}
+          {props.formState}
+          {' '}
+          Milestone Step
+          {' '}
+        </h1>
 
-                <Formik onSubmit={onSubmit} initialValues={{ description: initialValue }} validationSchema={MilestoneSchema}>
-                    <Form>
-                        <Paper elevation={1} className={classes.paper}>
-                            <Field
-                                as={TextField}
-                                name="description"
-                                placeholder="Please write your description here"
-                                maxLength={300}
-                                multiline
-                            />
-                            <Button
-                                className={classes.submitButton}
-                                size="small"
-                                type="submit"
-                                color="primary"
-                                variant="contained"
-                                autoFocus
-                            >
-                                Submit
-                            </Button>
-                        </Paper>
-                    </Form>
-                </Formik>
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={props.initialValues}
+          validationSchema={MilestoneSchema}
+        >
+          <Form>
+            <Paper elevation={1} className={classes.paper}>
+              <Field
+                as={TextField}
+                name="description"
+                placeholder="Please write your milestone description here"
+                maxLength={300}
+                multiline
+              />
+              <Button
+                className={classes.submitButton}
+                type="submit"
+                color="primary"
+                variant="contained"
+                autoFocus
+              >
+                Submit
+              </Button>
+            </Paper>
+          </Form>
+        </Formik>
 
-            </div>
-        </Container>
-    );
+      </div>
+    </Container>
+  );
 };
 
 MilestoneForm.propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-  
-  export default MilestoneForm;
+  onSubmitHandler: PropTypes.func.isRequired,
+  initialValues: PropTypes.object,
+  formState: PropTypes.string,
+};
+MilestoneForm.defaultProps = {
+  initialValues: {
+    description: '',
+  },
+  formState: 'New',
+};
+
+export default MilestoneForm;
