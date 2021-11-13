697c7149fe207c24b06b921f80ba666904209413
unknown <joanna.bienkowska8@gmail.com>
2021-11-13 17:22:35 +0100

    Meeting notes and details (no backend)

--- src/screens/index.js
+++ src/screens/index.js
@@ -28,6 +28,7 @@ import UseToken from './UseToken';
 import EditProjectFormDashboard from './ProjectForm/EditFormDashboard';
 import ChangePassword from './Auth/ChangePassword';
 import MeetingList from './shared/components/MeetingList';
+import MeetingDetail from './shared/components/MeetingDetail';
 
 const App = () => {
   const { token, setToken } = UseToken();
@@ -62,6 +63,9 @@ const App = () => {
         <Route path="/calendar" component={CalendarMain} />
         <Route path="/change-password" component={ChangePassword} />
         <Route path="/milestones/:IdProject" component={Milestone} />
+        <Route path="/meeting_notes" component={MeetingList} />
+        <Route path="/meeting_details" component={MeetingDetail} />
+
       </Switch>
     </BrowserRouter>
 
